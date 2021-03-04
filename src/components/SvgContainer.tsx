435051264970a8e926730fc396acb47add051029
Bobby Tatum <bobbyt9927@yahoo.com>
2021-03-04 13:01:59 -0800

    added icon & splash logos

--- src/components/SvgContainer.tsx
+++ src/components/SvgContainer.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { SvgXml } from 'react-native-svg';
 
+// to define the types for SvgContainer props
 interface ISvgContainer {
-    xml: string,
-    height: string,
-    width: string,
+  xml: string;
+  height: string;
+  width: string;
 }
 
-export default function SvgContainer({xml, height, width}: ISvgContainer): JSX.Element{
-    return (
-        <SvgXml xml={xml} width={width} height={height} />
-    );
+export default function SvgContainer({
+  xml,
+  height,
+  width,
+}: ISvgContainer): JSX.Element {
+  return <SvgXml xml={xml} width={width} height={height} />;
 }
