b63feb25685295c49f662fefe65d1614ce22066f
Bobby Tatum <bobbyt9927@yahoo.com>
2021-03-09 17:54:01 -0800

    added release channel example

--- src/environment.ts
+++ src/environment.ts
@@ -6,20 +6,24 @@ import Constants from 'expo-constants';
  * the env var values. This can then be returned via the getEnvVars function.
  */
 const ENV = {
-  dev: {
-    apolloURI: 'localhost:4000/graphql',
-  },
-  staging: {}, // sample object for a 'staging' release channel
+    dev: {
+        apolloURI: 'localhost:4000/graphql',
+    },
+    staging: {}     // sample object for a 'staging' release channel
 };
 
 /*
  * Uses Expo release channels to determine env var values.
  * As more release channels are added (ex: staging, prod), add
- * a if/else-if block to return the property in ENV corresponding to
+ * a if/else-if block to return the property in ENV corresponding to 
  * the release channel.
  */
 const getEnvVars = (env = Constants.manifest.releaseChannel) => {
-  return ENV.dev;
-};
+    if (__DEV__) {
+        return ENV.dev;
+    } else if (env === 'staging') {
+        return ENV.staging;
+    }
+}
 
 export default getEnvVars;
