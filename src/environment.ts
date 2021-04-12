e93a80705d6d2c1b351ff4d77826204f20d9d1fa
Bobby <bobbyt9927@yahoo.com>
2021-04-11 22:40:32 -0700

    fixed env vars logic so it is no longer possible to resolve to an empty object

--- src/environment.ts
+++ src/environment.ts
@@ -6,24 +6,29 @@ import Constants from 'expo-constants';
  * the env var values. This can then be returned via the getEnvVars function.
  */
 const ENV = {
-    dev: {
-        apolloURI: 'localhost:4000/graphql',
-    },
-    staging: {}     // sample object for a 'staging' release channel
+  dev: {
+    apolloURI: 'localhost:4000/graphql',
+  },
+  staging: {}, // object would contain the env vars for a release channel titled 'staging'
 };
 
 /*
  * Uses Expo release channels to determine env var values.
  * As more release channels are added (ex: staging, prod), add
- * a if/else-if block to return the property in ENV corresponding to 
+ * a if/else-if block to return the property in ENV corresponding to
  * the release channel.
  */
 const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+  return ENV.dev;
+
+  /* Sample logic to determine env vars when multiple release channels are present:
+
     if (__DEV__) {
         return ENV.dev;
     } else if (env === 'staging') {
         return ENV.staging;
     }
-}
+    */
+};
 
 export default getEnvVars;
