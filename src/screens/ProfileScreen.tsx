2cc9cd1b8e9e141af0c356c4c35f66b8c9ad9ab8
Hana Kim <hsk003@ucsd.edu>
2021-04-19 02:29:57 -0700

    profile page ui

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable import/prefer-default-export */
 import React, { useContext } from 'react';
-import { Button, View } from 'react-native';
+import { Text, Button, View, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Auth } from 'aws-amplify';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { AppContext } from '../contexts/AppContext';
+import styles from '../styles/ProfileScreenStyles';
 
 export function ProfileScreen() {
   const { setAppState } = useContext(AppContext);
+
+  const name = 'John Smith';
+  const bio = 'Member since 2021';
+  const badgeNumber = 10;
 
   const signOut = async () => {
     try {
@@ -17,8 +25,91 @@ export function ProfileScreen() {
   };
 
   return (
-    <View>
-      <Button title="Sign Out" onPress={signOut} />
-    </View>
+    <KeyboardAwareScrollView
+      keyboardOpeningTime={0}
+      extraScrollHeight={25}
+      enableOnAndroid
+      contentContainerStyle={{ flexGrow: 1, height: 800 }}
+      scrollToOverflowEnabled
+    >
+      <View style={styles.backgroundGray}>
+        <View style={styles.backgroundBlue} />
+        <View style={styles.settings}>
+          <TouchableOpacity onPress={signOut}>
+            <Ionicons
+              name="information-circle-outline"
+              size={20}
+              color="white"
+            />
+          </TouchableOpacity>
+          <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>
+            PROFILE
+          </Text>
+          <TouchableOpacity onPress={signOut}>
+            <Ionicons name="settings" size={20} color="white" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.profileContainer}>
+          <Image
+            style={styles.image}
+            source={require('../images/profile-pic.png')}
+          />
+          <View>
+            <Text style={styles.name}>{name}</Text>
+          </View>
+          <View>
+            <Text style={styles.bio}>{bio}</Text>
+          </View>
+        </View>
+        <View style={styles.mainContainer}>
+          <View style={styles.mainLayout}>
+            <View style={styles.numberCircle}>
+              <Text style={styles.number}>{badgeNumber}</Text>
+            </View>
+            <Text style={styles.badgeText}>Total Badges Collected</Text>
+          </View>
+          <View style={styles.line} />
+          <Text style={styles.homeTitle}>SWITCH HOME</Text>
+          <View style={styles.homesLayout}>
+            <TouchableOpacity onPress={signOut}>
+              <View style={styles.homeCard}>
+                <View style={styles.homeSpacing}>
+                  <Text>Mountain</Text>
+                  <View style={styles.homeColor} />
+                </View>
+                <Text style={styles.address}>
+                  11 Mountain Ave Fresno, CA 92122
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={signOut}>
+              <View style={styles.homeCard}>
+                <View style={styles.homeSpacing}>
+                  <Text>Mountain</Text>
+                  <View style={styles.homeColor} />
+                </View>
+                <Text style={styles.address}>
+                  11 Mountain Ave Fresno, CA 92122
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={signOut}>
+              <View style={styles.homeCard}>
+                <View style={styles.homeSpacing}>
+                  <Text>Mountain</Text>
+                  <View style={styles.homeColor} />
+                </View>
+                <Text style={styles.address}>
+                  11 Mountain Ave Fresno, CA 92122
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <Button title="Sign Out" onPress={signOut} />
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
+
+export default ProfileScreen;
---
+++ src/styles/ProfileScreenStyles.tsx
@@ -0,0 +1,120 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  address: {
+    fontSize: 12,
+  },
+  backgroundBlue: {
+    position: 'absolute',
+    width: '100%',
+    height: '35%',
+    backgroundColor: '#86C5BE',
+  },
+  backgroundGray: {
+    justifyContent: 'flex-start',
+    backgroundColor: '#fafafa',
+    height: '100%',
+  },
+  badgeText: {
+    color: '#353935',
+    fontSize: 20,
+  },
+  bio: {
+    color: 'white',
+    fontSize: 13.5,
+    textAlign: 'center',
+  },
+  homeTitle: {
+    paddingTop: 15,
+    paddingLeft: 5,
+    paddingBottom: 15,
+  },
+  homesLayout: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  homeCard: {
+    margin: 8,
+    padding: 10,
+    height: 100,
+    width: 140,
+    borderRadius: 5,
+    backgroundColor: '#fafafa',
+  },
+  homeColor: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#6ba177',
+  },
+  homeSpacing: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  image: {
+    borderRadius: 60,
+    height: 70,
+    marginTop: 80,
+    marginBottom: 20,
+    width: 70,
+  },
+  line: {
+    marginLeft: 3,
+    marginRight: 3,
+    borderBottomColor: '#BFE3D5',
+    borderBottomWidth: 0.5,
+  },
+  mainContainer: {
+    position: 'absolute',
+    top: '30%',
+    alignSelf: 'center',
+    height: 460,
+    width: '90%',
+    borderRadius: 5,
+    paddingLeft: '2.5%',
+    backgroundColor: 'white',
+  },
+  mainLayout: {
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  name: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  number: {
+    padding: 15,
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  numberCircle: {
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 8,
+    marginRight: 15,
+    borderRadius: 50,
+    backgroundColor: '#6ba177',
+  },
+  profileContainer: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginBottom: 12,
+  },
+  settings: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    alignItems: 'center',
+    width: '100%',
+    paddingTop: '10%',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  },
+});
+
+export default styles;
