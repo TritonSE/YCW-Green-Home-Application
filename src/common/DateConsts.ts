5827ac12c85c588809f53031024867eb40823c8d
unsoupervised <37908451+unsoupervised@users.noreply.github.com>
2021-04-30 00:31:18 -0700

    feature: Add common/consts

--- src/common/DateConsts.ts
+++ src/common/DateConsts.ts
@@ -7,13 +7,15 @@
 
 export type Meridiem = 'AM' | 'PM';
 
+/**
+ * Important & Common Time units we can use (if ever).
+ */
 export const MS_PER_SEC = 1000;
 export const SEC_PER_MIN = 60;
 export const MIN_PER_HOUR = 60;
 export const HOUR_PER_DAY = 24;
 export const DAYS_PER_WEEK = 7;
 export const MONTHS_PER_YEAR = 12;
-export const US_PER_MS = 1000; // microseconds in a millisecond
 
 export const SEC_PER_HOUR = SEC_PER_MIN * MIN_PER_HOUR;
 export const SEC_PER_DAY = SEC_PER_HOUR * HOUR_PER_DAY;
---
+++ src/common/Sizes.ts
@@ -0,0 +1,11 @@
+export default {
+  topPadding: {
+    ios: 68,
+    android: 40,
+  },
+  text: {
+    title: 20,
+    subtitle: 16,
+    subSubtitle: 12,
+  },
+};
