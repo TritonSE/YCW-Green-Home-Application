6c0f4aec5f8a8deb038f8c08dac2719db3e1c982
Advay Sharma <sharmaadvay0@gmail.com>
2021-05-04 10:46:07 -0700

    clears user context on sign out

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -3,13 +3,31 @@ import React, { useContext } from 'react';
 import { Button, View } from 'react-native';
 import { Auth } from 'aws-amplify';
 import { AppContext } from '../contexts/AppContext';
+import { UserContext } from '../contexts/UserContext';
 
 export function ProfileScreen() {
   const { setAppState } = useContext(AppContext);
+  const { setUserState } = useContext(UserContext);
 
   const signOut = async () => {
     try {
       await Auth.signOut();
+      setUserState({
+        _deleted: null,
+        _lastChangedAt: 0,
+        _version: 1,
+        createdAt: '',
+        displayName: '',
+        homes: {
+          items: [],
+          nextToken: null,
+          startedAt: null,
+        },
+        id: '',
+        owner: '',
+        updatedAt: '',
+        username: '',
+      });
       setAppState('Auth');
     } catch (error) {
       console.error('Error signing out: ', error);
