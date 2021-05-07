f708ee81c41721cec98c27f7b85653cca86abfd4
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-05-07 11:03:03 -0700

    Remove dotenv from lambda code

--- amplify/backend/function/questionslist/src/config.js
+++ amplify/backend/function/questionslist/src/config.js
@@ -1,7 +1,3 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 const Config = {
   region: process.env.REGION,  
   tableName: `Question-${process.env.API_YOURCLEARWAYAPI_GRAPHQLAPIIDOUTPUT}-${process.env.ENV}`,
