89821c45c09471887a372e18078ad91e9ea1e306
Bobby <bobbyt9927@yahoo.com>
2021-05-11 18:35:04 -0700

    response conteext created

---
+++ src/contexts/ResponseContext.tsx
@@ -0,0 +1,76 @@
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+} from 'react';
+
+// responseState will have an array of response objects
+// response objects have id, homeId, QuestionID, home, no Question!!!, answer, createdAt
+interface ResponseState {
+  responseState: {
+    items: [
+      {
+        id: string;
+        homeID: string;
+        questionID: string;
+        // home: Home type (??)
+        // question: Question type (??)
+        answer: string;
+        createdAt: string;
+      },
+    ];
+  };
+  setResponseState:
+    | Dispatch<
+        SetStateAction<{
+          items: [
+            {
+              id: string;
+              homeID: string;
+              questionID: string;
+              // home: Home type (??)
+              // question: Question type (??)
+              answer: string;
+              createdAt: string;
+            },
+          ];
+        }>
+      >
+    | (() => void);
+}
+
+// should this be an empty array?
+const initialState: ResponseState = {
+  responseState: {
+    items: [
+      {
+        id: '',
+        homeID: '',
+        questionID: '',
+        answer: '',
+        createdAt: '',
+      },
+    ],
+  },
+  setResponseState: () => {}, // eslint-disable-line @typescript-eslint/no-empty-function
+};
+
+export const ResponseContext = createContext<ResponseState>(initialState);
+
+export const ResponseProvider: React.FC = ({ children }) => {
+  const [responseState, setResponseState] = useState(
+    initialState.responseState,
+  );
+
+  return (
+    <ResponseContext.Provider
+      value={{
+        responseState,
+        setResponseState,
+      }}
+    >
+      {children}
+    </ResponseContext.Provider>
+  );
+};
