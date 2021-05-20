496e067f122a38337b54f72e013e0a2e9815f50f
thaigillespie <ttgilles@ucsd.edu>
2021-05-20 15:28:50 -0700

    Changed dimension of Insulated Cellular Shades and Smart Thermostat badges

--- src/assets/badges/svgXMLs/insulCellularShades.ts
+++ src/assets/badges/svgXMLs/insulCellularShades.ts
@@ -1,17 +1,16 @@
-const insulatedCellularShadesXML = `<svg width="76" height="70" viewBox="0 0 76 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M37.9462 70C58.4422 70 75.0574 54.33 75.0574 35C75.0574 15.67 58.4422 0 37.9462 0C17.4502 0 0.834961 15.67 0.834961 35C0.834961 54.33 17.4502 70 37.9462 70Z" fill="#324A5E"/>
-<path d="M37.946 51.5357C47.6291 51.5357 55.4788 44.1325 55.4788 35.0003C55.4788 25.868 47.6291 18.4648 37.946 18.4648C28.2628 18.4648 20.4131 25.868 20.4131 35.0003C20.4131 44.1325 28.2628 51.5357 37.946 51.5357Z" fill="#FFD05B"/>
-<path d="M41.6868 53.3542L37.9464 61.4566L34.2061 53.3542C35.4334 53.5747 36.6607 53.685 37.9464 53.685C39.2321 53.685 40.5179 53.5747 41.6868 53.3542Z" fill="#FFD05B"/>
-<path d="M41.6868 16.6458C40.4595 16.4253 39.2321 16.3151 37.9464 16.3151C36.6607 16.3151 35.3749 16.4253 34.2061 16.6458L37.9464 8.54346L41.6868 16.6458Z" fill="#FFD05B"/>
-<path d="M18.134 35.0002C18.134 36.2128 18.2509 37.4254 18.4847 38.5829L9.89355 35.0002L18.4847 31.4727C18.2509 32.575 18.134 33.7876 18.134 35.0002Z" fill="#FFD05B"/>
-<path d="M57.4077 38.5829C57.6415 37.4254 57.7584 36.2679 57.7584 35.0002C57.7584 33.7876 57.6415 32.575 57.4077 31.4727L65.9988 35.0002L57.4077 38.5829Z" fill="#FFD05B"/>
-<path d="M26.8416 50.4884L18.0752 53.7404L21.4649 45.4727C22.926 47.4569 24.7377 49.1656 26.8416 50.4884Z" fill="#FFD05B"/>
-<path d="M57.8169 16.26L54.4272 24.5277C53.0246 22.5435 51.2128 20.8348 49.1089 19.512L57.8169 16.26Z" fill="#FFD05B"/>
-<path d="M26.8416 19.512C24.7377 20.8348 22.926 22.5435 21.5233 24.5277L18.0752 16.26L26.8416 19.512Z" fill="#FFD05B"/>
-<path d="M57.8167 53.7404L49.0503 50.5436C51.1542 49.2207 52.966 47.5121 54.3686 45.5278L57.8167 53.7404Z" fill="#FFD05B"/>
-<path d="M44.2579 34.1732H39.1734L40.7514 25.189L31.6343 35.8268H36.7188L35.1409 44.811L44.2579 34.1732Z" fill="white"/>
-</svg>
-`;
+const insulatedCellularShadesXML = `<svg width="71" height="70" viewBox="0 0 71 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M35.835 70C55.1649 70 70.835 54.33 70.835 35C70.835 15.67 55.1649 0 35.835 0C16.505 0 0.834961 15.67 0.834961 35C0.834961 54.33 16.505 70 35.835 70Z" fill="#324A5E"/>
+<path d="M35.8348 51.5357C44.967 51.5357 52.3702 44.1325 52.3702 35.0003C52.3702 25.868 44.967 18.4648 35.8348 18.4648C26.7025 18.4648 19.2993 25.868 19.2993 35.0003C19.2993 44.1325 26.7025 51.5357 35.8348 51.5357Z" fill="#FFD05B"/>
+<path d="M39.3627 53.3542L35.8352 61.4566L32.3076 53.3542C33.4651 53.5747 34.6226 53.685 35.8352 53.685C37.0478 53.685 38.2604 53.5747 39.3627 53.3542Z" fill="#FFD05B"/>
+<path d="M39.3627 16.6458C38.2053 16.4253 37.0478 16.3151 35.8352 16.3151C34.6226 16.3151 33.41 16.4253 32.3076 16.6458L35.8352 8.54346L39.3627 16.6458Z" fill="#FFD05B"/>
+<path d="M17.1501 35.0002C17.1501 36.2128 17.2603 37.4254 17.4808 38.5829L9.37842 35.0002L17.4808 31.4727C17.2603 32.575 17.1501 33.7876 17.1501 35.0002Z" fill="#FFD05B"/>
+<path d="M54.1895 38.5829C54.4099 37.4254 54.5202 36.2679 54.5202 35.0002C54.5202 33.7876 54.4099 32.575 54.1895 31.4727L62.2918 35.0002L54.1895 38.5829Z" fill="#FFD05B"/>
+<path d="M25.3619 50.4884L17.0942 53.7404L20.2911 45.4727C21.669 47.4569 23.3777 49.1656 25.3619 50.4884Z" fill="#FFD05B"/>
+<path d="M54.5754 16.26L51.3785 24.5277C50.0557 22.5435 48.347 20.8348 46.3628 19.512L54.5754 16.26Z" fill="#FFD05B"/>
+<path d="M25.3619 19.512C23.3777 20.8348 21.669 22.5435 20.3462 24.5277L17.0942 16.26L25.3619 19.512Z" fill="#FFD05B"/>
+<path d="M54.5749 53.7404L46.3071 50.5436C48.2914 49.2207 50 47.5121 51.3229 45.5278L54.5749 53.7404Z" fill="#FFD05B"/>
+<path d="M41.7878 34.1732H36.9926L38.4808 25.189L29.8823 35.8268H34.6776L33.1894 44.811L41.7878 34.1732Z" fill="white"/>
+</svg>`;
 
 const grayInsulatedCellularShadesXML = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg
@@ -22,16 +21,16 @@ const grayInsulatedCellularShadesXML = `<?xml version="1.0" encoding="UTF-8" sta
    xmlns="http://www.w3.org/2000/svg"
    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   width="76"
+   width="71"
    height="70"
-   viewBox="0 0 76 70"
+   viewBox="0 0 71 70"
    fill="none"
    version="1.1"
-   id="svg382"
+   id="svg62"
    sodipodi:docname="gray_insulatedCellularShades.svg"
    inkscape:version="1.0.2-2 (e86c870879, 2021-01-15)">
   <metadata
-     id="metadata388">
+     id="metadata68">
     <rdf:RDF>
       <cc:Work
          rdf:about="">
@@ -43,94 +42,94 @@ const grayInsulatedCellularShadesXML = `<?xml version="1.0" encoding="UTF-8" sta
     </rdf:RDF>
   </metadata>
   <defs
-     id="defs386">
+     id="defs66">
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter392">
+       id="filter72">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix390" />
+         id="feColorMatrix70" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter396">
+       id="filter76">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix394" />
+         id="feColorMatrix74" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter400">
+       id="filter80">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix398" />
+         id="feColorMatrix78" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter404">
+       id="filter84">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix402" />
+         id="feColorMatrix82" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter408">
+       id="filter88">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix406" />
+         id="feColorMatrix86" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter412">
+       id="filter92">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix410" />
+         id="feColorMatrix90" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter416">
+       id="filter96">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix414" />
+         id="feColorMatrix94" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter420">
+       id="filter100">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix418" />
+         id="feColorMatrix98" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter424">
+       id="filter104">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix422" />
+         id="feColorMatrix102" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter428">
+       id="filter108">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix426" />
+         id="feColorMatrix106" />
     </filter>
     <filter
        style="color-interpolation-filters:sRGB;"
        inkscape:label="Greyscale"
-       id="filter432">
+       id="filter112">
       <feColorMatrix
          values="0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0.21 0.72 0.072 0 0 0 0 0 1 0 "
-         id="feColorMatrix430" />
+         id="feColorMatrix110" />
     </filter>
   </defs>
   <sodipodi:namedview
@@ -144,70 +143,70 @@ const grayInsulatedCellularShadesXML = `<?xml version="1.0" encoding="UTF-8" sta
      inkscape:pageshadow="2"
      inkscape:window-width="1920"
      inkscape:window-height="1001"
-     id="namedview384"
+     id="namedview64"
      showgrid="false"
-     inkscape:zoom="11.857143"
-     inkscape:cx="38"
+     inkscape:zoom="11.442857"
+     inkscape:cx="35.5"
      inkscape:cy="35"
      inkscape:window-x="-9"
      inkscape:window-y="-9"
      inkscape:window-maximized="1"
-     inkscape:current-layer="svg382" />
+     inkscape:current-layer="svg62" />
   <path
-     d="M37.9462 70C58.4422 70 75.0574 54.33 75.0574 35C75.0574 15.67 58.4422 0 37.9462 0C17.4502 0 0.834961 15.67 0.834961 35C0.834961 54.33 17.4502 70 37.9462 70Z"
+     d="M35.835 70C55.1649 70 70.835 54.33 70.835 35C70.835 15.67 55.1649 0 35.835 0C16.505 0 0.834961 15.67 0.834961 35C0.834961 54.33 16.505 70 35.835 70Z"
      fill="#324A5E"
-     id="path360"
-     style="filter:url(#filter432)" />
+     id="path40"
+     style="filter:url(#filter112)" />
   <path
-     d="M37.946 51.5357C47.6291 51.5357 55.4788 44.1325 55.4788 35.0003C55.4788 25.868 47.6291 18.4648 37.946 18.4648C28.2628 18.4648 20.4131 25.868 20.4131 35.0003C20.4131 44.1325 28.2628 51.5357 37.946 51.5357Z"
+     d="M35.8348 51.5357C44.967 51.5357 52.3702 44.1325 52.3702 35.0003C52.3702 25.868 44.967 18.4648 35.8348 18.4648C26.7025 18.4648 19.2993 25.868 19.2993 35.0003C19.2993 44.1325 26.7025 51.5357 35.8348 51.5357Z"
      fill="#FFD05B"
-     id="path362"
-     style="filter:url(#filter428)" />
+     id="path42"
+     style="filter:url(#filter108)" />
   <path
-     d="M41.6868 53.3542L37.9464 61.4566L34.2061 53.3542C35.4334 53.5747 36.6607 53.685 37.9464 53.685C39.2321 53.685 40.5179 53.5747 41.6868 53.3542Z"
+     d="M39.3627 53.3542L35.8352 61.4566L32.3076 53.3542C33.4651 53.5747 34.6226 53.685 35.8352 53.685C37.0478 53.685 38.2604 53.5747 39.3627 53.3542Z"
      fill="#FFD05B"
-     id="path364"
-     style="filter:url(#filter424)" />
+     id="path44"
+     style="filter:url(#filter104)" />
   <path
-     d="M41.6868 16.6458C40.4595 16.4253 39.2321 16.3151 37.9464 16.3151C36.6607 16.3151 35.3749 16.4253 34.2061 16.6458L37.9464 8.54346L41.6868 16.6458Z"
+     d="M39.3627 16.6458C38.2053 16.4253 37.0478 16.3151 35.8352 16.3151C34.6226 16.3151 33.41 16.4253 32.3076 16.6458L35.8352 8.54346L39.3627 16.6458Z"
      fill="#FFD05B"
-     id="path366"
-     style="filter:url(#filter420)" />
+     id="path46"
+     style="filter:url(#filter100)" />
   <path
-     d="M18.134 35.0002C18.134 36.2128 18.2509 37.4254 18.4847 38.5829L9.89355 35.0002L18.4847 31.4727C18.2509 32.575 18.134 33.7876 18.134 35.0002Z"
+     d="M17.1501 35.0002C17.1501 36.2128 17.2603 37.4254 17.4808 38.5829L9.37842 35.0002L17.4808 31.4727C17.2603 32.575 17.1501 33.7876 17.1501 35.0002Z"
      fill="#FFD05B"
-     id="path368"
-     style="filter:url(#filter416)" />
+     id="path48"
+     style="filter:url(#filter96)" />
   <path
-     d="M57.4077 38.5829C57.6415 37.4254 57.7584 36.2679 57.7584 35.0002C57.7584 33.7876 57.6415 32.575 57.4077 31.4727L65.9988 35.0002L57.4077 38.5829Z"
+     d="M54.1895 38.5829C54.4099 37.4254 54.5202 36.2679 54.5202 35.0002C54.5202 33.7876 54.4099 32.575 54.1895 31.4727L62.2918 35.0002L54.1895 38.5829Z"
      fill="#FFD05B"
-     id="path370"
-     style="filter:url(#filter412)" />
+     id="path50"
+     style="filter:url(#filter92)" />
   <path
-     d="M26.8416 50.4884L18.0752 53.7404L21.4649 45.4727C22.926 47.4569 24.7377 49.1656 26.8416 50.4884Z"
+     d="M25.3619 50.4884L17.0942 53.7404L20.2911 45.4727C21.669 47.4569 23.3777 49.1656 25.3619 50.4884Z"
      fill="#FFD05B"
-     id="path372"
-     style="filter:url(#filter408)" />
+     id="path52"
+     style="filter:url(#filter88)" />
   <path
-     d="M57.8169 16.26L54.4272 24.5277C53.0246 22.5435 51.2128 20.8348 49.1089 19.512L57.8169 16.26Z"
+     d="M54.5754 16.26L51.3785 24.5277C50.0557 22.5435 48.347 20.8348 46.3628 19.512L54.5754 16.26Z"
      fill="#FFD05B"
-     id="path374"
-     style="filter:url(#filter404)" />
+     id="path54"
+     style="filter:url(#filter84)" />
   <path
-     d="M26.8416 19.512C24.7377 20.8348 22.926 22.5435 21.5233 24.5277L18.0752 16.26L26.8416 19.512Z"
+     d="M25.3619 19.512C23.3777 20.8348 21.669 22.5435 20.3462 24.5277L17.0942 16.26L25.3619 19.512Z"
      fill="#FFD05B"
-     id="path376"
-     style="filter:url(#filter400)" />
+     id="path56"
+     style="filter:url(#filter80)" />
   <path
-     d="M57.8167 53.7404L49.0503 50.5436C51.1542 49.2207 52.966 47.5121 54.3686 45.5278L57.8167 53.7404Z"
+     d="M54.5749 53.7404L46.3071 50.5436C48.2914 49.2207 50 47.5121 51.3229 45.5278L54.5749 53.7404Z"
      fill="#FFD05B"
-     id="path378"
-     style="filter:url(#filter396)" />
+     id="path58"
+     style="filter:url(#filter76)" />
   <path
-     d="M44.2579 34.1732H39.1734L40.7514 25.189L31.6343 35.8268H36.7188L35.1409 44.811L44.2579 34.1732Z"
+     d="M41.7878 34.1732H36.9926L38.4808 25.189L29.8823 35.8268H34.6776L33.1894 44.811L41.7878 34.1732Z"
      fill="white"
-     id="path380"
-     style="filter:url(#filter392)" />
+     id="path60"
+     style="filter:url(#filter72)" />
 </svg>`;
 
 export { insulatedCellularShadesXML, grayInsulatedCellularShadesXML };
