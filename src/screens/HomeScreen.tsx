8b2e1b4f835c74310e9ca1e4c3743f90fbc186b9
Advay Sharma <sharmaadvay0@gmail.com>
2021-05-26 00:23:03 -0700

    added home page logic

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,55 @@
 /* eslint-disable import/prefer-default-export */
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
+import SvgContainer from '../components/SvgContainer';
+import { QuestionContext } from '../contexts/QuestionsContext';
+import { ResponseContext } from '../contexts/ResponseContext';
 
-export function HomeScreen() {
+export const HomeScreen = () => {
+  const { questionState } = useContext(QuestionContext);
+  const { responseState } = useContext(ResponseContext);
+
+  const positiveResponse = responseState.items.filter(
+    response => response.answer === 'Yes',
+  );
+
+  const sortedResponses = positiveResponse.sort((a, b) => {
+    const date1 = new Date(a.createdAt);
+    const date2 = new Date(b.createdAt);
+    if (date1 > date2) {
+      return -1;
+    }
+    return 1;
+  });
+
+  const badgesList = sortedResponses.map(response => {
+    const questionObject = questionState.items.find(
+      question => question.id === response.questionID,
+    );
+    return questionObject?.title;
+  });
+
+  const completedTextList = sortedResponses.map(response => {
+    const questionObject = questionState.items.find(
+      question => question.id === response.questionID,
+    );
+    return questionObject?.rewardText;
+  });
+
   return (
     <View>
-      <Text>Welcome to the home screen!</Text>
+      {badgesList.map(badge => {
+        if (badge !== undefined) {
+          return <SvgContainer badgeTitle={badge} height="40" width="40" />;
+        }
+        return <Text>No Badge Found</Text>;
+      })}
+      {completedTextList.map(text => {
+        if (text !== undefined) {
+          return <Text>{text}</Text>;
+        }
+        return <Text>No Badge Found</Text>;
+      })}
     </View>
   );
-}
+};
